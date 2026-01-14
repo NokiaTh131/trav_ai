c8ab249256012444bf489e36eeb1220ef20dd136
NokiaTh131 <nokiath131@gmail.com>
2026-01-15 00:57:07 +0700

    refactor: testing memvid find in long query

--- test/test_memvid.py
+++ test/test_memvid.py
@@ -7,7 +7,8 @@ path = "app/mem/thai_guide.mv2"
 if os.path.exists(path):
     mem = use("langchain", path)
 
-    results = mem.find("Island in Pattaya", k=3)
+    # results = mem.ask("Island in Pattaya", k=3)
+    results = mem.find("what is the top 3 place to visit in Pattaya", k=3)
     # pprint.pp([[h["title"], h["labels"]] for h in results["hits"]])  # pyright: ignore[reportTypedDictNotRequiredAccess]
-    pprint.pp(results["hits"])
-    pprint.pp(mem.timeline(limit=5))
+    pprint.pp(results)
+    # pprint.pp(mem.timeline(limit=5))
